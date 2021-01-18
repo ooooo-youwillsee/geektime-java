29359b98708d031451e2cdc8fe476d3feb030c4f
ooooo <297872913@qq.com>
2021-01-18 22:37:49 +0800

    init

---
+++ src/main/java/com/geektime/GeektimeJavaApplication.java
@@ -0,0 +1,13 @@
+package com.geektime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeektimeJavaApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(GeektimeJavaApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/com/geektime/GeektimeJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geektime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeektimeJavaApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
